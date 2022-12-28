184d6d71b419ae30073328a60d63207024e03aa3
Sevi-py <73383494+Sevi-py@users.noreply.github.com>
2022-12-28 19:12:45 +0100

    Explained every fucking thing in Fahrt3

--- 2023/Fahrt3.py
+++ 2023/Fahrt3.py
@@ -16,13 +16,13 @@ hub.motion_sensor.reset_yaw_angle()
 
 
 #Black line stuff
-def onBlackLine():
+def onBlackLine(): #Returns True if both color sensors are seeing black
     if color1.get_color() and color2.get_color() == "black":
         return True
     else:
         return False
 
-def moveUntilBlackLine(speed: int, delay = 1, steering :int = 0):
+def moveUntilBlackLine(speed: int, delay :float = 1, steering :int = 0): #Starts the motor pair and leaves it running until onBlackLine returns True
     motor_pair.start(speed=speed, steering = steering)
     wait_for_seconds(delay)
     wait_until(onBlackLine,target_value=True)
@@ -37,35 +37,19 @@ def yaw(targetYaw :int, speed :int=15):
 
 
 #Code for Driving
-motor_pair.move(62, "cm", steering=-6, speed=60)
+motor_pair.move(62, "cm", steering=-6, speed=60) #Drive a smooth curve around "Trichter"
 
-#yaw(targetYaw=90, speed=20)
-#motor_pair.move(5, "cm", steering=0, speed=70)
-#yaw(targetYaw=0, speed=20)
+moveUntilBlackLine(50) #Drives straight until the black line in the middle 
+motor_pair.move(3, "cm", speed=20) #Correction because the color sensors are not placed on the same position as the wheels
 
-moveUntilBlackLine(50)
-motor_pair.move(3, "cm", speed=20)
+yaw(90) #Spins until the robot is parallel to the black line in the middle
 
-yaw(90)
+moveUntilBlackLine(40) #Drives until the black line before the hand
+motor_pair.move(20, "cm", speed=30) #Drives forward into the hand
+motor_pair.move(9, "cm", speed=-15) #Drives backwards, so the hand gets triggered
+motor_pair.move(11, "cm", speed=-100) #Drives backwards, so the module for the hand disconnects from the robot
 
-if color1.get_color == "black":
-    yaw(targetYaw=0, speed=20)
-    motor_pair.move(3, "cm", speed=15)
-    yaw(targetYaw=90, speed=20)
-elif color2.get_color == "black":
-    yaw(targetYaw=180, speed=20)
-    motor_pair.move(3, "cm", speed=15)
-    yaw(targetYaw=90, speed=20)
+yaw(23) #Spins so the robot is able to drive between the hand and the other fucking thing
 
-moveUntilBlackLine(40)
-motor_pair.move(20, "cm", speed=30)
-
-#yaw(76)
-
-motor_pair.move(9, "cm", speed=-15)
-motor_pair.move(11, "cm", speed=-100)
-
-yaw(23)
-
-motor_pair.move(10, "cm", speed=30)
-motor_pair.move(30, "cm", steering=-69, speed=30)
+motor_pair.move(10, "cm", speed=30) #Drives a bit forward, so the robot is able to do the next line without crashing into the hand
+motor_pair.move(30, "cm", steering=-69, speed=30) #Drives a curve, so the robot is able to collect the batteries on the side
