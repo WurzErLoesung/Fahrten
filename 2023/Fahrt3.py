1ea2951c7be7230324d4770eff953d02d369a746
Sevi-py <73383494+Sevi-py@users.noreply.github.com>
2022-12-28 18:31:15 +0100

    cleaned up Fahrt3

--- 2023/Fahrt3.py
+++ 2023/Fahrt3.py
@@ -13,29 +13,28 @@ motor_pair = MotorPair("B", "F")
 color1 = ColorSensor("E")
 color2 = ColorSensor("A")
 
+def yaw(targetYaw :int, speed :int=15):
+    motor_pair.start(speed=speed, steering=100 * (min(1, max(hub.motion_sensor.get_yaw_angle() - targetYaw, -1))))
+    wait_until(hub.motion_sensor.get_yaw_angle,target_value=targetYaw)
+    motor_pair.stop()
+
 def onBlackLine():
     if color1.get_color() and color2.get_color() == "black":
         return True
     else:
         return False
+
 hub.motion_sensor.reset_yaw_angle()
 
-#motor_pair.start(speed=15, steering=-100)
-#wait_until(hub.motion_sensor.get_yaw_angle,target_value=11)
-#motor_pair.stop()
+#yaw(11)
 
 motor_pair.move(62, "cm", steering=-6, speed=60)
 
-#motor_pair.start(speed=20, steering=-100)
-#wait_until(hub.motion_sensor.get_yaw_angle,target_value=90)
-#motor_pair.stop()
+#yaw(targetYaw=90, speed=20)
 
 #motor_pair.move(5, "cm", steering=0, speed=70)
 
-#motor_pair.start(speed=20, steering=100)
-#wait_until(hub.motion_sensor.get_yaw_angle,target_value=0)
-#motor_pair.stop()
-
+#yaw(targetYaw=0, speed=20)
 
 motor_pair.start(speed=50)
 
@@ -45,26 +44,16 @@ motor_pair.stop()
 
 motor_pair.move(3, "cm", speed=20)
 
-motor_pair.start(speed=15, steering=-100)
-wait_until(hub.motion_sensor.get_yaw_angle,target_value=90)
-motor_pair.stop()
+yaw(90)
 
 if color1.get_color == "black":
-    motor_pair.start(speed=20, steering=100)
-    wait_until(hub.motion_sensor.get_yaw_angle,target_value=0)
-    motor_pair.stop()
+    yaw(targetYaw=0, speed=20)
     motor_pair.move(3, "cm", speed=15)
-    motor_pair.start(speed=20, steering=-100)
-    wait_until(hub.motion_sensor.get_yaw_angle,target_value=90)
-    motor_pair.stop()
+    yaw(targetYaw=90, speed=20)
 elif color2.get_color == "black":
-    motor_pair.start(speed=20, steering=-100)
-    wait_until(hub.motion_sensor.get_yaw_angle,target_value=180)
-    motor_pair.stop()
+    yaw(targetYaw=180, speed=20)
     motor_pair.move(3, "cm", speed=15)
-    motor_pair.start(speed=20, steering=100)
-    wait_until(hub.motion_sensor.get_yaw_angle,target_value=90)
-    motor_pair.stop()
+    yaw(targetYaw=90, speed=20)
 
 motor_pair.start(speed=40)
 wait_for_seconds(2)
@@ -72,17 +61,13 @@ wait_until(onBlackLine,target_value=True)
 
 motor_pair.move(20, "cm", speed=30)
 
-#motor_pair.start(speed=15, steering=100)
-#wait_until(hub.motion_sensor.get_yaw_angle,target_value=76)
-#motor_pair.stop()
+#yaw(76)
 
 motor_pair.move(9, "cm", speed=-15)
 
 motor_pair.move(11, "cm", speed=-100)
 
-motor_pair.start(speed=15, steering=100)
-wait_until(hub.motion_sensor.get_yaw_angle,target_value=23)
-motor_pair.stop()
+yaw(23)
 
 motor_pair.move(10, "cm", speed=30)
 
