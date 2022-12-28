8aad5a05fec5cc091f3b20b49e14a475856d4e97
Sevi-py <73383494+Sevi-py@users.noreply.github.com>
2022-12-28 18:55:31 +0100

    cleaned up and commented Fahrt3

--- 2023/Fahrt3.py
+++ 2023/Fahrt3.py
@@ -1,23 +1,21 @@
 # Bei Fragen an Severin wenden
 # Fahrt ist zuständig für Hand, Ölinsel und die Energiezellen daneben.
 
+
 from spike import PrimeHub, LightMatrix, Button, StatusLight, ForceSensor, MotionSensor, Speaker, ColorSensor, App, DistanceSensor, Motor, MotorPair
 from spike.control import wait_for_seconds, wait_until, Timer
 from math import *
 
+
+#Initialising
 hub = PrimeHub()
-
-hub.light_matrix.show_image('HAPPY')
-
 motor_pair = MotorPair("B", "F")
 color1 = ColorSensor("E")
 color2 = ColorSensor("A")
+hub.motion_sensor.reset_yaw_angle()
 
-def yaw(targetYaw :int, speed :int=15):
-    motor_pair.start(speed=speed, steering=100 * (min(1, max(hub.motion_sensor.get_yaw_angle() - targetYaw, -1))))
-    wait_until(hub.motion_sensor.get_yaw_angle,target_value=targetYaw)
-    motor_pair.stop()
 
+#Black line stuff
 def onBlackLine():
     if color1.get_color() and color2.get_color() == "black":
         return True
@@ -30,20 +28,22 @@ def moveUntilBlackLine(speed: int, delay = 1, steering :int = 0):
     wait_until(onBlackLine,target_value=True)
     motor_pair.stop()
 
-hub.motion_sensor.reset_yaw_angle()
 
-#yaw(11)
+#Easy to use spinning function
+def yaw(targetYaw :int, speed :int=15):
+    motor_pair.start(speed=speed, steering=100 * (min(1, max(hub.motion_sensor.get_yaw_angle() - targetYaw, -1))))
+    wait_until(hub.motion_sensor.get_yaw_angle,target_value=targetYaw)
+    motor_pair.stop()
 
+
+#Code for Driving
 motor_pair.move(62, "cm", steering=-6, speed=60)
 
 #yaw(targetYaw=90, speed=20)
-
 #motor_pair.move(5, "cm", steering=0, speed=70)
-
 #yaw(targetYaw=0, speed=20)
 
 moveUntilBlackLine(50)
-
 motor_pair.move(3, "cm", speed=20)
 
 yaw(90)
@@ -58,17 +58,14 @@ elif color2.get_color == "black":
     yaw(targetYaw=90, speed=20)
 
 moveUntilBlackLine(40)
-
 motor_pair.move(20, "cm", speed=30)
 
 #yaw(76)
 
 motor_pair.move(9, "cm", speed=-15)
-
 motor_pair.move(11, "cm", speed=-100)
 
 yaw(23)
 
 motor_pair.move(10, "cm", speed=30)
-
 motor_pair.move(30, "cm", steering=-69, speed=30)
