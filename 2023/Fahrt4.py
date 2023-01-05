05be89f23d1e7e7cac2daded30d97bc64f7022d5
Simon Unger <72646433+TheSimonCraft@users.noreply.github.com>
2023-01-05 15:58:16 +0100

    Update Fahrt4.py
    Version of 5.1.2023

--- 2023/Fahrt4.py
+++ 2023/Fahrt4.py
@@ -2,94 +2,26 @@ from spike import PrimeHub, LightMatrix, Button, StatusLight, ForceSensor, Motio
 from spike.control import wait_for_seconds, wait_until, Timer
 from math import *
 
-hub = PrimeHub()
-
-print("Ready")
 force = ForceSensor('D')
 wait_until(force.is_pressed)
 wait_for_seconds(0.5)
 
-def relative_yaw(target_yaw: int):
-    yaw(current_yaw + target_yaw)
-
-def yaw(target_yaw: int):
-    drive.start(100 * (min(1, max(MotionSensor.get_yaw_angle() - target_yaw, -1))), 10)
-    wait_until(MotionSensor.get_yaw_angle, target_value=target_yaw)
-    drive.stop()
-    return MotionSensor.get_yaw_angle()
-
-# INITIALIZATION
 hub = PrimeHub()
-MotionSensor = hub.motion_sensor
-MotionSensor.reset_yaw_angle()
 drive = MotorPair('B', 'F')
-action = Motor('C')
-ColorLeft = ColorSensor('E')
-ColorRight = ColorSensor('A')
-drive.set_stop_action("hold")
 
-# Variable Initialization
-start_yaw = MotionSensor.get_yaw_angle()
-current_yaw = start_yaw
-# Adjust Default Speed
-default_speed = 35
-# Adjust Speed to OIL VEHICLE
-speed_vehicle = 35
-# Adjust way to OIL VEHICLE
-way_to_oil = 55
-# Adjust straight way at start of Part 2
-straight_part_two = 3
-# Asjust way to CENTER
-way_to_center = 101
-# Adjust steering to CENTER
-steering_to_center = 12
-# Adjust way to drop at CENTER
-way_drop_center = 25
-# Adjust yaw for drop at CENTER
-yaw_drop_center = 0
-# Adjust way after drop at CENTER
-way_after_center = 5.5
-# Adjust yaw to SOLAR PARK
-yaw_after_center = -45
-# Adjust way to SOLAR PARK
-way_to_solar = 70
-# Adjust speed to SOLAR PARK & GOAL
-speed_to_goal = 70
+# move to storage task and stop slowly
+drive.set_default_speed(50)
+drive.set_stop_action('coast')
+drive.move(33.5, "cm", -5, 40)
+drive.move(39, "cm", 5, 30)
+drive.move(1.5, "cm", 5, 10)
+wait_for_seconds(0.3)
 
-# Collect OIL VEHICLE
-current_yaw = yaw(1)
-drive.set_default_speed(default_speed)
-drive.set_stop_action("coast")
-drive.move(way_to_oil/2, "cm", 0, speed_vehicle)
-yaw(0)
-drive.move(way_to_oil/2, "cm", 0, speed_vehicle)
-wait_for_seconds(0.25)
-drive.start(speed=-speed_vehicle)
+# collect drawer from storage
+drive.move(5, "cm", 0, -4)
+drive.move(-16)
+drive.move(-15, "cm", -88, 40)
+drive.start(10, 50)
+
 wait_until(force.is_pressed)
-wait_for_seconds(0.5)
 drive.stop()
-
-# Wait until Button is pressed
-wait_until(force.is_pressed)
-wait_for_seconds(0.5)
-
-# PART 2
-
-drive.move(straight_part_two)
-
-# Drive to CENTER
-drive.move(way_to_center, "cm", steering_to_center)
-
-# Drop at CENTER
-drive.move(-way_drop_center)
-current_yaw = yaw(yaw_drop_center)
-
-# Move to SOLAR PARK
-drive.move(way_after_center)
-current_yaw = yaw(yaw_after_center)
-drive.set_stop_action("hold")
-drive.move(65, "cm", 0, speed_to_goal)
-action.run_for_seconds(0.5, -50)
-drive.start(speed=speed_to_goal)
-wait_for_seconds(0.2)
-drive.move(3, "cm", 0, -100)
