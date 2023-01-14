6ea0bf9e74d09927e23c7ebbb573a93517e56fd9
Simon Unger <72646433+TheSimonCraft@users.noreply.github.com>
2023-01-14 09:22:49 +0100

    Update Fahrt4.py
    Put default speed into a var

--- 2023/Fahrt4.py
+++ 2023/Fahrt4.py
@@ -9,22 +9,25 @@ force = ForceSensor('D')
 wait_until(force.is_pressed)
 wait_for_seconds(0.5)
 
+# VAR INITIALIZATION
+default_speed = 50
+
 hub = PrimeHub()
 drive = MotorPair('B', 'F')
 
 # move to storage task and stop slowly
-drive.set_default_speed(50)
+drive.set_default_speed(default_speed)
 drive.set_stop_action('coast')
-drive.move(33.5, "cm", -5, 40)
-drive.move(39, "cm", 5, 30)
+drive.move(33.5, "cm", -5)
+drive.move(39, "cm", 5)
 drive.move(1.5, "cm", 5, 10)
 wait_for_seconds(0.3)
 
 # collect drawer from storage
 drive.move(5, "cm", 0, -4)
 drive.move(-16)
-drive.move(-15, "cm", -88, 40)
-drive.start(10, 50)
+drive.move(-15, "cm", -88)
+drive.start(10)
 
 wait_until(force.is_pressed)
 drive.stop()
