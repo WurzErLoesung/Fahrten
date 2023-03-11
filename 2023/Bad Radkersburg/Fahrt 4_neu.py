b264abf0d1a0d0c3013a2e762488c2af1fa362e7
Sebi19 <s.schreitter@gmx.at>
2023-03-11 17:29:21 +0100

    Fahrt 4 klappt jetzt bisschen besser

--- 2023/Bad Radkersburg/Fahrt 4_neu.py
+++ 2023/Bad Radkersburg/Fahrt 4_neu.py
@@ -36,74 +36,53 @@ ColorRight = ColorSensor('A')
 # VAR INITIALIZATION
 start_yaw = MotionSensor.get_yaw_angle()
 current_yaw = start_yaw
-default_speed = 50
+default_speed = 60
 
-# move to storage task and stop slowly
 drive.set_default_speed(default_speed)
 drive.set_stop_action('coast')
-#drive.move(33.5, "cm", -13)
-#drive.move(37, "cm", 12)
-#drive.move(5, "cm", 12, 10)
-#wait_for_seconds(0.3)
 
-#drive to storage
+# drive to storage task
 drive.move(15, "cm", 0, 50)
+yaw(0)
+action.run_for_degrees(-110, 50)
 yaw(30)
 drive.move(34, "cm", 0, 50)
 yaw(0)
-drive.move(15, "cm", 0, 50)
-drive.move(10, "cm", 0, 20)
 
-#adjust for oil
-drive.move(-21)
-current_yaw = yaw(45)
-drive.move(20)
-current_yaw = yaw(90)
-drive.move(-4)
+# drop energy
+drive.move(25, "cm", 0, 40)
+yaw(0)
 
-#do oil
+#readjust for oil
+drive.move(-16, "cm", 0)
+yaw(40)
+drive.move(17, "cm", 0)
+yaw(-90)
+wait_for_seconds(0.5)
+yaw(-90)
+
+#oil task
+drive.move(10, "cm", 0)
 for i in range(4):
-    drive.start(0, -50)
-    wait_for_seconds(0.5)
-    drive.stop()
-    drive.move(5)
+    yaw(-90)
+    drive.move(1, "seconds", 0, 100)
+    drive.move(-5, "cm")
 
-#adjust for energy
-drive.move(5, "cm")
-drive.move(-8, "cm", -60)
-drive.move(12, "cm")
-drive.move(5, "cm", -20)
-current_yaw = yaw(90)
-drive.move(-5, "cm")
+#collect energies
+yaw(-90)
+drive.move(-10, "cm")
+yaw(-179)
+action.run_for_degrees(110, 50)
 
-#collect energy 1 + 2
-action.run_for_seconds(1.5, -100)
-
-#collect energy 3
-drive.move(22, "cm", 7)
-drive.move(5, "cm", -100)
-yaw(150)
-
-#drive to center
-drive.move(26, "cm", 10)
-yaw(179)
-drive.move(25, "cm")
-
-#drive to hand
-drive.start(0, -50)
-action.start(100)
-wait_for_seconds(2)
-action.stop()
-drive.stop()
-
-#drive back to center
-drive.start(5, 50)
-action.start(-100)
-wait_for_seconds(1)
-drive.stop()
-wait_for_seconds(1.5)
-action.stop()
-drive.move(15, "cm", -5)
-
-wait_until(force.is_pressed)
-drive.stop()
+MotionSensor.reset_yaw_angle()
+yaw(58)
+drive.move(-8, "cm", 0, 20)
+yaw(53)
+action.run_for_degrees(-110, 50)
+drive.move(-8, "cm", 0, 20)
+yaw(50)
+action.run_for_degrees(110, 50)
+yaw(135)
+drive.move(-32, "cm")
+action.run_for_degrees(-110, 50)
+yaw(0)
