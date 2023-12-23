21e7c5fadcd05cdcee5caef70f16de2e067e7b3d
Johni12M <jonathan.metz999@icloud.com>
2023-12-23 15:41:50 +0100

    Optimization Fahrt 2

--- 2024/Fahrt 2.py
+++ 2024/Fahrt 2.py
@@ -1,3 +1,22 @@
+#Ausrichtung 1 (Am Anfang von der Fahrt)
+#Rote Base
+#Gabelstapler unten
+#L eingehängt
+#Maxerl mit Kopf nach Süden und Vorderteil nach Westen laden
+#x-Achse West-Ost, y-Achse Süd-Nord
+#Räder nach Norden
+#mitte vom linken Rad: x=2, y=10,5
+
+
+#Ausrichtung 2 (In der Mitte von der Fahrt)
+#Rote Base
+#Gabelstapler unten
+#L eingehängt
+#Maxerl mit Kopf nach Süden und Vorderteil nach Westen laden
+#x-Achse West-Ost, y-Achse Süd-Nord
+#Räder nach Norden
+#Mitte des rechten Rades: x = 14, y = 10,5
+
 from spike import PrimeHub, LightMatrix, Button, StatusLight, ForceSensor, MotionSensor, Speaker, ColorSensor, App, DistanceSensor, Motor, MotorPair
 from spike.control import wait_for_seconds, wait_until, Timer
 from spike.operator import *
@@ -13,10 +32,8 @@ def yaw(target_yaw: int = 0):
 def relative_yaw(yaw_step: int):
     yaw(gyro.get_yaw_angle() + yaw_step)
 
-def drop_figure():
-    drop_figure_time = 1.3
-    action_front.run_for_seconds(drop_figure_time, speed=-100)
-    action_front.run_for_seconds(drop_figure_time, speed=100)
+
+    
 
 
 #Initalizing
@@ -36,19 +53,24 @@ drive.set_stop_action("brake")
 drive.set_default_speed(50) #Standard Schnelligkeit 50%
 
 #3d Kino
-drive.move(21,steering=32)
+drop_figure_time = 1.3
+drive.move(24,steering=32)
 drive.move(7, steering=-100)
 yaw(0)
-drive.move(-3)
-yaw(-8)
-drive.move(13,steering=38,speed=100)
-
+drive.move(-24)
 wait_for_seconds(5)
-drive.move(35, steering = -15) #Fährt zu Popcorn
-drop_figure() #lässt erste Figur fallen
-drive.move(-2)
-drive.move(32, steering = 47) #Fährt zu Aufgabe Szenenwechsel
-drop_figure()
+drive.move(34, steering = -11)#Fährt zu Popcorn
+
+drive_left.run_for_rotations(-0.3)
+action_front.run_for_rotations(-0.7)
+wait_for_seconds(1)
+drive.move(16) #Fährt zu Aufgabe Szenenwechsel
+
+
+action_front.run_for_rotations(-0.7) #Zweite Figur bei Szenenwechsel abladen
+wait_for_seconds(0.5)
+drive.move(-15,steering=30)
+"""
 #Aufgabe Szenenwechsel
 value = (2 if color.get_color() == 'yellow' else 1)
 relative_yaw(180)
@@ -68,3 +90,4 @@ drive.move(15,steering=-24)
 drive.move(30)
 yaw(0)
 drive.move(20)
+"""
