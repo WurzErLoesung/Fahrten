dbde032e617efe8f4cb4755cd2574a586eba672f
TheSimonCraft <72646433+TheSimonCraft@users.noreply.github.com>
2023-12-28 10:09:20 +0100

    Update Fahrt 1.py

--- 2024/Fahrt 1.py
+++ 2024/Fahrt 1.py
@@ -1,3 +1,8 @@
+#Ausrichtung:
+# x = 9.5 Rechter Reifen Mitte
+# y = Bande
+# t = ~9-10sec
+
 from spike import PrimeHub, LightMatrix, Button, StatusLight, ForceSensor, MotionSensor, Speaker, ColorSensor, App, DistanceSensor, Motor, MotorPair
 from spike.control import wait_for_seconds, wait_until, Timer
 from spike.operator import *
@@ -8,7 +13,6 @@ drive = MotorPair('E', 'A')
 action_back = Motor('B')
 gyro = hub.motion_sensor
 gyro.reset_yaw_angle()
-wait_for_seconds(10)
 
 def yaw(target_yaw: int = 0):
     direction = min(1, max(-1, gyro.get_yaw_angle() - target_yaw))# Wählt für alle Gradzahlen unter dem target_yaw -1, für alle Zahlen darüber +1 aus
@@ -17,46 +21,14 @@ def yaw(target_yaw: int = 0):
     wait_until(gyro.get_yaw_angle, equal_to, target_yaw)            # Wartet, bis die Richtung 0° ist
     drive.stop()
 
+#Funktion fürs Drehen aus der aus der aktuellen Position um eine Anzahl an Grad
 def relative_yaw(yaw_step: int):
     yaw(gyro.get_yaw_angle() + yaw_step)
 action_front = Motor('D')
 
-hub.light_matrix.show_image('HAPPY')
-
-action_back.run_for_rotations(-0.5)
-
-
-drive.move(-35, speed=60)
-yaw(0)
-action_back.run_for_rotations(0.5)
-drive.move(-4.5)
-
-action_back.run_for_rotations(-0.6)
-drive.move(3)
-
-
-drive.set_stop_action('coast')
-drive.move(-15, steering=-100, speed=100)
-
-
-drive.move(-4, steering=100, speed=100)
-
-
-yaw(0)
-
-drive.move(44, steering=0)
-
-wait_for_seconds(4)
-
-# Mischpult (Motorgesteuerte Unterstützung notwendig)
-drive.start(speed=60)
-wait_for_seconds(1.9) # Previous: 1.9
-action_front.run_for_seconds(0.7, speed=90)
-wait_for_seconds(0.3)
-
-drive.stop()
-drive.move(-0.2)
-drive.move(-10, 'cm', 100, 10)
-drive.move(-30)
-yaw(-70)
-drive.move(70)
+drive.move(45, speed=65, steering=-6)
+drive.move(-14, speed=30)
+drive.move(10, steering=100)
+action_front.run_for_rotations(-6, speed=100)
+drive.move(-10, steering=100)
+drive.move(-30, speed=100)
