c5a13e4011b22eb0889b3cb3964edb475add0840
BRANDSTETTER Emil <brandsemi@keimgasse.at>
2023-12-28 11:26:46 +0100

    Finished Fahrt3.py

--- 2024/Fahrt3.py
+++ 2024/Fahrt3.py
@@ -20,7 +20,7 @@ def yaw(target_yaw: int = 0):
     print(target_yaw)
     direction = min(1, max(-1, (gyro.get_yaw_angle() - target_yaw + 180) % 360 - 180))# Wählt für alle Gradzahlen unter dem target_yaw -1, für alle Zahlen darüber +1 aus
     steering = 100 * direction                                    # Volle Kanne nach links oder nach rechts (100 oder -100)
-    print(steering)
+    #print(steering)
     drive.start(steering, 10)                                    # Langsam, aber stark in Richtung drehen
     wait_until(gyro.get_yaw_angle, equal_to, target_yaw)            # Wartet, bis die Richtung 0° ist
     drive.stop()
@@ -30,16 +30,14 @@ def relative_yaw(yaw_step: int):
 action_front = Motor('D')
 
 
-
 drive.move(-35, speed=100)
 drive.move(33, steering=50)
-yaw(-134)
-drive.move(48, speed=60)
+yaw(-132)
+drive.move(47.5, speed=80)
 drive.move(-1)
 relative_yaw(-88)
-drive.move(39)
-drive.move(30, steering=100, speed=100)
-drive.move(-7)
+drive.move(37)
+drive.move(24, steering=100, speed=100)
+drive.move(-2.5)
 action_front.run_for_rotations(1)
-relative_yaw(8)
 drive.move(75, speed=100)
