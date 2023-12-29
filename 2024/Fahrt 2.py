c02448b713c7756742ae5c2b2652c5b0d649d4a6
1SimonHofer <148616086+1SimonHofer@users.noreply.github.com>
2023-12-29 13:17:03 +0100

    Update Fahrt 2.py
    Continues(Didn't write that right)

--- 2024/Fahrt 2.py
+++ 2024/Fahrt 2.py
@@ -65,10 +65,11 @@ drive.set_default_speed(75) #Standard Schnelligkeit 50%
 
 drive.move(37, steering = -9, speed=70)#Fährt zu Popcorn
 drive_left.run_for_rotations(-0.1)
+
 action_front.run_for_rotations(-0.7)
-drive.move(30, steering=36, speed=70)
+drive.move(32 steering=36, speed=90)
 drive.move(-5)
-wait_for_seconds(0.5)
+wait_for_seconds(0.8)
 drive.move(9)
 drive.move(-5)
 drive.move(-10, steering=70)
@@ -84,14 +85,5 @@ action_front.run_for_rotations(2.5)
 action_front.run_for_rotations(-1.1)
 action_front.run_for_rotations(1.1)
 action_back.run_for_rotations(7)
-
-
-# drive.move(20, steering=-50, speed=70)
-
-
-# drive.move(5, steering=-100, speed=50)
-# drive.move(30, steering=-50)
-# action_front.run_for_rotations(-0.6)
-# drive.move(6, steering=-100)
-# action_back.run_for_rotations(3)
+drive.move(-10, steering=-60)
 
