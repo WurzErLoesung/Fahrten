6150517353a1c235757f7c0669eaf02184de7b39
BRANDSTETTER Emil <brandsemi@keimgasse.at>
2024-01-02 16:47:19 +0100

    modified Fahrt2

--- 2024/Fahrt 2.py
+++ 2024/Fahrt 2.py
@@ -1,3 +1,8 @@
+from spike import PrimeHub, LightMatrix, Button, StatusLight, ForceSensor, MotionSensor, Speaker, ColorSensor, App, DistanceSensor, Motor, MotorPair
+from spike.control import wait_for_seconds, wait_until, Timer
+from spike.operator import *
+from math import *
+
 #Ausrichtung 1 (Am Anfang von der Fahrt)
 #Rote Base
 #Gabelstapler unten
@@ -17,23 +22,19 @@
 #Räder nach Norden
 #Mitte des rechten Rades: x = 14, y = 10,5
 
-from spike import PrimeHub, LightMatrix, Button, StatusLight, ForceSensor, MotionSensor, Speaker, ColorSensor, App, DistanceSensor, Motor, MotorPair
-from spike.control import wait_for_seconds, wait_until, Timer
-from spike.operator import *
-from math import *
-
 def yaw(target_yaw: int = 0):
     direction = min(1, max(-1, gyro.get_yaw_angle() - target_yaw))
     steering = 100*direction
-    drive.start(steering, speed=10)
-    wait_until(gyro.get_yaw_angle, equal_to, target_yaw)
+    drive.start(steering, speed=8)
+    wait_until(gyro.get_yaw_angle, equal_to, target_yaw + 4*direction)
     drive.stop()
 
 def relative_yaw(yaw_step: int):
     yaw(gyro.get_yaw_angle() + yaw_step)
 
-
-
+def drop_figure():
+    action_front.run_for_rotations(-0.7, speed=80)
+    wait_for_seconds(0.3)
 
 
 #Initalizing
@@ -49,41 +50,43 @@ color = ColorSensor ("C")
 
 gyro.reset_yaw_angle()
 
-drive.set_stop_action("brake")
-drive.set_default_speed(75) #Standard Schnelligkeit 50%
+drive.set_stop_action("hold")
+drive.set_default_speed(75) #Standard Schnelligkeit 75%
 
 # 3d Kino
 # drop_figure_time = 1.3
-# drive.move(24,steering=36, speed=100)
-# drive.move(9, steering=-100, speed=80)
-# yaw(0)
+# drive.move(24,steering=36, speed=100) #Fährt zum Drachen
+# drive.move(9, steering=-100, speed=80) #Löst den Drachen aus
 # drive.move(-15, speed=100)
 
 
-# wait_for_seconds(5)
+# wait_for_seconds(5) #neu Ausrichten für 2. Teil
 
 
-drive.move(37, steering = -9, speed=70)#Fährt zu Popcorn
-drive_left.run_for_rotations(-0.1)
-
-action_front.run_for_rotations(-0.7)
-drive.move(32 steering=36, speed=90)
-drive.move(-5)
-wait_for_seconds(0.8)
-drive.move(9)
-drive.move(-5)
-drive.move(-10, steering=70)
-drive.move(10, steering=-20, speed=60)
-relative_yaw(45)
-drive.move(20, steering=-30)
-action_front.run_for_rotations(-0.6)
-drive.move(10, steering=-30, speed=50)
-drive.move(15, steering=-35, speed=70)
-drive.move(22, steering=-40)
-action_front.run_for_rotations(-0.6)
-action_front.run_for_rotations(2.5)
-action_front.run_for_rotations(-1.1)
-action_front.run_for_rotations(1.1)
-action_back.run_for_rotations(7)
-drive.move(-10, steering=-60)
-
+drive.move(38, steering = -8, speed=70)#Fährt zu Popcorn
+yaw()
+drop_figure()
+drive.move(17)
+yaw(-45)
+drive.move(15, speed=40)
+drop_figure()
+drive.move(-5.5)
+wait_for_seconds(2.5)
+drive.move(15, speed=40)
+drive.move(-8)
+yaw()
+drive.move(-13, speed=70)
+yaw(45)
+drive.move(38, steering=-2)
+drop_figure()
+drive.move(-18)
+yaw(90)
+drive.move(45)
+yaw(45)
+drive.move(24)
+yaw(90)
+action_back.run_for_rotations(8)
+drive.move(-3)
+action_back.run_for_rotations(-9)
+yaw()
+drive.move(15)
