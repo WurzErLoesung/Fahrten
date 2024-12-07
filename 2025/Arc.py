955a4f8b5c34b6c30225819826685ddd86574cda
MightBeHuman <simon.unger2007@gmail.com>
2024-12-07 13:22:44 +0100

    Forklift Arc function Uploaded for debugging on windows machine

--- 2025/Arc.py
+++ 2025/Arc.py
@@ -3,6 +3,7 @@ from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor, ForceSenso
 from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
 from pybricks.robotics import DriveBase
 from pybricks.tools import wait, StopWatch
+from umath import sin, cos, pi
 
 hub = PrimeHub()
 
@@ -20,13 +21,27 @@ def action_arc(radius: int, angle: int, velocity: int):
     mleft = Motor(Port.B)
     mright = Motor(Port.F, direction=Direction.COUNTERCLOCKWISE)
     db = DriveBase(mleft, mright, 56, 113)
+    
+    velocity_cm = (phi * 2 * radius * pi) / 360
+
+    velocity_forklift = (3 * velocity_cm) / 360
+    velocity_movement = (56 * pi * velocity_cm) / 360
 
     t_max = angle / velocity
 
     s = StopWatch()
-    t = 0
-    while s.time() < t_final:
+    while (t:=s.time()) < t_final:
         phi = t * (angle / t_max)
+        vertical_ratio, horizontal_ratio = velocity * cos(phi), velocity * sin(phi)
+        ma.start(vertical_ratio * velocity_forklift)
+        db.start(horizontal_ratio * velocity_movement)
+
+    db.stop()
+    ma.stop()
+
         
-        dt = s.time() - t
-        t = s.time()
+if __name__ == "__main__":
+    action_arc(10, 90, 10)
+        
+
+
