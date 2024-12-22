db5b09830540ab993d605b78d7889b2b614639dd
Johni12M <jonathan.metz999@icloud.com>
2024-12-22 13:35:32 +0100

    update drive 2

--- 2025/drive2.py
+++ 2025/drive2.py
@@ -4,9 +4,10 @@ from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
 from pybricks.robotics import DriveBase
 from pybricks.tools import wait, StopWatch, run_task, multitask
 from action_arc import action_arc
+from yaw import Yaw
 
 hub = PrimeHub()
-# Motoren initialisieren 
+# Motoren initialisieren
 left_motor = Motor(Port.B, positive_direction=Direction.CLOCKWISE) 
 right_motor = Motor(Port.F, positive_direction=Direction.COUNTERCLOCKWISE)
 
@@ -15,6 +16,9 @@ action_back = Motor(Port.A, positive_direction=Direction.COUNTERCLOCKWISE)
 
 ultra = UltrasonicSensor(Port.E)
 color = ColorSensor(Port.D)
+
+yaw = Yaw(hub, left_motor, right_motor)
+
 watch = StopWatch()
 
 #DriveBase initialisieren
@@ -27,34 +31,38 @@ StopWatch = watch
 hub.speaker.beep()
 
 #Korallenbaum
-drive_base.straight(197)
+drive_base.straight(192)
 action_back.run_angle(500, 250)
 #action_arc(drive_base, -1, action_back, 27, 200, 90, 30, 15)
 action_arc(drive_base, -1, action_back, 20, 182, 90, 30, 20)
 action_back.run_angle(500, -430)
-drive_base.straight(-100)
+drive_base.straight(-95)
 
 #zu Schiffwrack
-drive_base.turn(150)
-drive_base.straight(-300.3)
-drive_base.turn(-60)
+yaw(-135)
+drive_base.straight(-302)
+#drive_base.turn(-57)
+yaw(-90)
 drive_base.settings(100)
-drive_base.straight(-190)
-action_front.run_angle(1000, 50)
-drive_base.straight(-70)
-action_front.run_angle(1000, -50)
-drive_base.straight(-300)
-"""
+drive_base.straight(-193)
+action_front.run_angle(1000, 70)
+drive_base.straight(-55)
+action_front.run_angle(1000, -90)
+drive_base.straight(150)
 drive_base.settings(900)
-drive_base.straight(120)
 
 #Korallenbaum Teil 2
 drive_base.turn(-7)
-drive_base.straight(150)
+drive_base.straight(125)
 drive_base.straight(-80)
-drive_base.turn(-50)
-action_back.run_angle(500, -250)
-"""
+yaw(0)
+
+#Hai und Taucherin
+drive_base.straight(170)
+yaw(-73)
+action_back.run_angle(1250, -160)
+drive_base.straight(200)
+
 print("Fahrt 2 hat " + str(watch.time()/1000) + " Sekunden gedauert.")
 watch.reset()
 
