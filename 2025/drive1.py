52431a91f193386e5eb83ff1d011b14550daac52
Johni12M <148616249+Johni12M@users.noreply.github.com>
2024-12-23 15:52:03 +0100

    drive5

--- 2025/drive1.py
+++ 2025/drive1.py
@@ -3,9 +3,11 @@ from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor, ForceSenso
 from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
 from pybricks.robotics import DriveBase
 from pybricks.tools import wait, StopWatch, run_task
-
+from action_arc import action_arc
+from yaw import Yaw
 
 hub = PrimeHub()
+
 # Motoren initialisieren 
 left_motor = Motor(Port.E, positive_direction=Direction.CLOCKWISE) 
 right_motor = Motor(Port.A, positive_direction=Direction.COUNTERCLOCKWISE)
@@ -23,7 +25,6 @@ axle_track = 113
 drive_base = DriveBase(left_motor, right_motor, wheel_diameter, axle_track)
 drive_base.use_gyro(True) 
 drive_base.settings(straight_speed=900, straight_acceleration=500)
-
 hub.speaker.beep()
 
 drive_base.settings(900)
@@ -40,20 +41,25 @@ drive_base.straight(-300)
 
 #3 Schrimps + 1 Alge einsammeln
 drive_base.turn(18)
+
 #1. Schrimps
 drive_base.straight(130)
 drive_base.turn(40)
+
 #Alge
 drive_base.straight(110)
 drive_base.turn(-25)
+
 #2. Schrimps
 drive_base.straight(140)
 drive_base.settings(turn_rate=40)
 drive_base.turn(40)
 drive_base.settings(900)
+
 #3. Schrimps
 drive_base.straight(120)
 drive_base.turn(-145)
+
 #Algenprobe
 drive_base.straight(70)
 action_front.run_angle(80, 200)
@@ -64,15 +70,18 @@ action_front.run_angle(80, -150)
 drive_base.turn(10)
 drive_base.straight(60)
 drive_base.turn(45)
+
 #zu Anglerfich
 drive_base.turn(-10)
 drive_base.straight(500)
 drive_base.turn(-55)
 drive_base.curve(200, 40)
+
 #zu Wasserprobe
 drive_base.straight(300)
 drive_base.turn(-10)
 drive_base.straight(70)
+
 #zu Meeresbodenprobe
 drive_base.straight(200)
 drive_base.turn(-30)
