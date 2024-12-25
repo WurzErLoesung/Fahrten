75bb9be2c80c8abcb0e3fcbbeddadf1414cb1878
Sebi19 <s.schreitter@gmx.at>
2024-12-26 00:01:24 +0100

    Modifizierte Fahrt 1, Korallenriff hinzugefügt

--- 2025/drive1_newrobot.py
+++ 2025/drive1_newrobot.py
@@ -6,6 +6,9 @@ from pybricks.tools import wait, StopWatch, run_task
 
 
 hub = PrimeHub()
+
+print(f"{hub.battery.voltage()/1000} Volt")
+
 # Motoren initialisieren
 left_motor = Motor(Port.B, positive_direction=Direction.COUNTERCLOCKWISE)
 right_motor = Motor(Port.F, positive_direction=Direction.CLOCKWISE)
@@ -22,65 +25,85 @@ axle_track = 113
 drive_base = DriveBase(left_motor, right_motor, wheel_diameter, axle_track)
 drive_base.use_gyro(True)
 
+
 watch = StopWatch()
 
 hub.speaker.beep()
 
 drive_base.settings(250)
 
-# zu der/die/das Kaktus
-drive_base.turn(-3.8)
-drive_base.straight(590)
-drive_base.settings(950)
-action_back.run_angle(200, -170)
+#für Roboter weiß
+
+#arm hochheben
+action_front.run_angle(200, -110) #200, -110
+
+#zu der/die/das Kaktus
+drive_base.turn(-4.5) #-4,5
+drive_base.straight(590) #590
+drive_base.settings(950) #950
+action_back.run_angle(200, -170) #200, -170
 
 #zu 1. Schrimm & Koralle
-drive_base.straight(-200)
-drive_base.turn(75)
-drive_base.straight(200)
+drive_base.straight(-200) #-200
+drive_base.turn(75) #75
+drive_base.straight(200) #200
 
 #2. Schrims
-drive_base.turn(-30)
-drive_base.straight(180)
-drive_base.turn(35)
-drive_base.straight(70)
+drive_base.turn(-30) #30
+drive_base.straight(195) #before 180
+drive_base.turn(35) #35
+drive_base.straight(70) #70
 
 #zu Karotte
-drive_base.turn(-120)
-drive_base.straight(120)
-action_back.run_angle(200, 190)
-drive_base.straight(-55)
-action_back.run_angle(200, -70)
-drive_base.straight(200)
-action_back.run_angle(200, -100)
+drive_base.turn(-124)#before -120
+drive_base.straight(120) #120
+action_back.run_angle(200, 190) #200, 190
+drive_base.straight(-60) #-55
+action_back.run_angle(200, -80) #200, -80
+action_front.run_angle(200, -50) #200, -50
+drive_base.straight(206) #200
+action_back.run_angle(200, -90) #200, -90
 
 #zu Anglerfisch
-drive_base.turn(-9.85) # before 10
-drive_base.straight(441.35) #before 441.4
+drive_base.turn(-3) # before 10
+drive_base.straight(400) #before 441.35
+drive_base.straight(25) #25
 
-drive_base.straight(-20) # bleibt somit nicht mehr hengen beim Anglerfisch
+drive_base.straight(-20) # bleibt somit nicht mehr hängen beim Anglerfisch
 
-# eingesammelte Sachen abstellen
-drive_base.turn(15) 
-drive_base.straight(313) #300
-drive_base.turn(-15)
-drive_base.straight(210)
+#eingesammelte Sachen abstellen
+drive_base.turn(13) #13
+drive_base.straight(255) #300
+drive_base.turn(-10.5) #-10.5
 
-#zu Bodenprobe
-drive_base.turn(-26.3) #before 26.28
+#zum Korallenriff
+drive_base.settings(150)
+drive_base.straight(240) #170
+#drive_base.settings(250)
+wait(300) #300
+action_front.run_angle(800, 150) #800, 150
+action_front.run_angle(100, -160) #100,-180
 
-action_back.run_angle(300, 85) #before 85
+#zum Anker
+drive_base.straight(-120) #100
+drive_base.turn(-34) #before 34
+action_back.run_angle(80, 79) #before 80,78
+drive_base.straight(-40) #-45
+drive_base.turn(-19) #-17
+drive_base.straight(-60) #60
 
-drive_base.straight(-215) #before -214
-action_back.run_angle(200, -90)
-drive_base.straight(310)
-drive_base.turn(-34.67) #before 34.65
+#Anker hoch
+action_back.run_angle(200, -90) #200, -90
 
-#zurück in rote Base
-drive_base.straight(513) #before 510
-drive_base.turn(26.4) #before 26.2
-drive_base.straight(290)
-hub.speaker.beep()
+#alles einsammeln
+drive_base.turn(50) #30
+drive_base.straight(270) #270
+drive_base.turn(-25) #-30
+drive_base.straight(120) #80
+drive_base.turn(-57) #-50
+drive_base.straight(20)
+drive_base.settings(700)
+drive_base.curve(600, 65)
 
 print("Fahrt 1 hat " + str(watch.time()/1000) + " Sekunden gedauert.")
 watch.reset()
