789e08f81e8718d11edaedfd5f0621dfdcde3e80
MightBeHuman <simon.unger2007@gmail.com>
2024-12-27 12:40:32 +0100

    Abstracted Motors and Sensors into a Singleton to avoid errors due to repetitive initialization

---
+++ 2025/pupdevices.py
@@ -0,0 +1,24 @@
+from pybricks.hubs import PrimeHub
+from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor
+from pybricks.parameters import Button, Color, Direction, Port
+from pybricks.robotics import DriveBase
+from pybricks.tools import wait, StopWatch
+
+
+class PupDevices(object):
+     def __new__(cls, *args, **kw):
+         if not hasattr(cls, '_instance'):
+             orig = super(PupDevices, cls)
+             cls._instance = orig.__new__(cls, *args, **kw)
+         return cls._instance
+
+    def __init__(self):
+        self.left_motor = Motor(Port.B)
+        self.right_motor = Motor(Port.F, positive_direction=Direction.COUNTERCLOCKWISE)
+        self.action_front = Motor(Port.C)
+        self.action_back = Motor(Port.A, positive_direction=Direction.COUNTERCLOCKWISE)
+        self.hub = PrimeHub()
+        self.imu = self.hub.imu
+        self.ultra = UltrasonicSensor(Port.E)
+        self.color = ColorSensor(Port.D)
+
