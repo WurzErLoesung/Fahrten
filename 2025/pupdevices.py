fb6a6e68722c99f22085fb7480dde7f5fb251b3a
MightBeHuman <simon.unger2007@gmail.com>
2024-12-27 15:34:59 +0100

    Fixed no attribute __new__ for super error

--- 2025/pupdevices.py
+++ 2025/pupdevices.py
@@ -6,19 +6,16 @@ from pybricks.tools import wait, StopWatch
 
 
 class PupDevices(object):
-     def __new__(cls, *args, **kw):
-         if not hasattr(cls, '_instance'):
-             orig = super(PupDevices, cls)
-             cls._instance = orig.__new__(cls, *args, **kw)
-         return cls._instance
+    def __new__(cls, *args, **kw):
+        if not hasattr(cls, '_instance'):
+            cls._instance = super(SingletonClass, cls).__new__(cls)
 
-     def __init__(self):
-         self.left_motor = Motor(Port.B)
-         self.right_motor = Motor(Port.F, positive_direction=Direction.COUNTERCLOCKWISE)
-         self.action_front = Motor(Port.C)
-         self.action_back = Motor(Port.A, positive_direction=Direction.COUNTERCLOCKWISE)
-         self.hub = PrimeHub()
-         self.imu = self.hub.imu
-         self.ultra = UltrasonicSensor(Port.E)
-         self.color = ColorSensor(Port.D)
-
+            cls._instance.left_motor = Motor(Port.B)
+            cls._instance.right_motor = Motor(Port.F, positive_direction=Direction.COUNTERCLOCKWISE)
+            cls._instance.action_front = Motor(Port.C)
+            cls._instance.action_back = Motor(Port.A, positive_direction=Direction.COUNTERCLOCKWISE)
+            cls._instance.hub = PrimeHub()
+            cls._instance.imu = cls._instance.hub.imu
+            cls._instance.ultra = UltrasonicSensor(Port.E)
+            cls._instance.color = ColorSensor(Port.D)
+        return cls._instance
