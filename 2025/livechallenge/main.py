5531cd62152d8839af5e13eff2b2988745789c96
Johni12M <148616249+Johni12M@users.noreply.github.com>
2025-03-28 09:57:04 +0100

    added some live challenge config

--- 2025/livechallenge/main.py
+++ 2025/livechallenge/main.py
@@ -4,28 +4,32 @@ from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
 from pybricks.robotics import DriveBase
 from pybricks.tools import wait, StopWatch, run_task
 from yaw import Yaw
+from PID import PID
 
-m = Motor(Port.A)
-action = Motor(Port.E)
-distance = UltrasonicSensor(Port.C)
-
+ml, mr = Motor(Port.B, positive_direction=Direction.COUNTERCLOCKWISE), Motor(Port.F)
+action = Motor(Port.C)
+#ultra = UltrasonicSensor(Port.D)
+pid = PID(mr, ml, 56)
+db = DriveBase(ml, mr, 56, 113)
 hub = PrimeHub()
-
 print(f"{hub.battery.voltage()/1000} Volt")
-
-watch = StopWatch()
-
-hub.speaker.beep()
+#watch = StopWatch()
 
 
-def live_challenge():
-    action.run_angle(400, -70)
-    action.run_angle(200, -80)
 
-    print("LiveChallenge Programm hat " + str(watch.time()/1000) + " Sekunden gedauert.")
-    watch.reset()
+async def live_challenge():
+    #Code here
+    hub.speaker.beep()
+    #db.settings(950)
+
+    await pid.straight(800, 400)
+
+    #db.straight(800)
+
+    #print("LiveChallenge Programm hat " + str(watch.time()/1000) + " Sekunden gedauert.")
+    #watch.reset()
 
 
 
 if __name__ == "__main__":
-    live_challenge()
+    run_task(live_challenge())
