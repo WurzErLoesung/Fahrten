68cdea496d9393cd88eb9085137208a977262440
Johni12M <148616249+Johni12M@users.noreply.github.com>
2025-08-17 11:16:59 +0200

    created new branch for tests of all kind

---
+++ 2026/test_drive_straight.py
@@ -0,0 +1,33 @@
+from pybricks.hubs import PrimeHub
+from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
+from pybricks.pupdevices import (ColorSensor, ForceSensor, Motor,
+                                 UltrasonicSensor)
+from pybricks.robotics import DriveBase
+from pybricks.tools import StopWatch, run_task, wait
+
+from pupdevices import PupDevices
+from yaw import Yaw
+
+hub = PrimeHub()
+
+print(f"{hub.battery.voltage()/1000} Volt")
+print(hub.system.name())
+
+watch = StopWatch
+
+hub.speaker.beep()
+
+
+def straight(pd):
+    pd.drive_base.use_gyro(False)
+    pd.imu.reset_heading(0)
+    pd.drive_base.settings(500, 400)
+    yaw = Yaw(hub, pd.left_motor, pd.right_motor, positive_direction=-1)
+    yield True
+
+    pd.drive_base.drive(100, 0)
+
+
+if __name__ == "__main__":
+    for element in drive1(PupDevices()):
+        pass
