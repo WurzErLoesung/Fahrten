50225054b3b95cdbeb4cb3fdc9c0932826247d16
Sebi19 <s.schreitter@gmx.at>
2025-08-18 18:15:32 +0200

    Adapt drive 4

--- 2026/drive4.py
+++ 2026/drive4.py
@@ -25,32 +25,42 @@ def drive4(pd):
     yaw = Yaw(hub, pd.left_motor, pd.right_motor, positive_direction=-1)
     yield True
 
-    pd.action_left.run_until_stalled(400, then=Stop.COAST, duty_limit=35)
+    pd.action_left.run_angle(200, 50)
 
-    pd.action_right.run_until_stalled(-400, then=Stop.COAST, duty_limit=35)
-    pd.drive_base.straight(390)
+    pd.action_right.run_until_stalled(-300, then=Stop.COAST, duty_limit=35)
+    pd.drive_base.straight(400)
+
 
     for i in range(3):
-        pd.action_right.run_angle(1500, 150)
-        wait(250)
-        pd.action_right.run_angle(1500, -170)
+        pd.action_right.run_angle(600, 150)
+        wait(500)
+        pd.action_right.run_angle(300, -160)
         wait(250)
     yield True
-    pd.action_right.run_until_stalled(-400, then=Stop.COAST, duty_limit=35)
+    pd.action_right.run_until_stalled(-300, then=Stop.COAST, duty_limit=50)
+    pd.action_left.run_angle(200, -50)
 
     pd.drive_base.straight(-100)
     yaw(-30)
     pd.drive_base.straight(460)
     yaw(60)
-    pd.drive_base.straight(100)
-    pd.action_right.run_angle(100, 150)
-    yaw(-40)
-    pd.action_right.run_angle(100, -120)
-    yaw(-30)
-    pd.drive_base.straight(-72)
+    pd.drive_base.straight(110)
+    pd.action_left.run_angle(200, 50)
+    pd.action_right.run_angle(100, 120)
+    yaw(20)
+    pd.drive_base.straight(-25)
+    yaw(-12)
+    pd.drive_base.straight(-50)
+    pd.action_right.run_angle(100, -90)
+    pd.action_left.run_angle(200, -50)
     yaw(90)
-    pd.drive_base.straight(350)
-    pd.drive_base.straight(-20)
+    pd.drive_base.straight(220)
+    pd.action_left.run_angle(200, 50)
+    pd.drive_base.straight(200)
+    pd.drive_base.straight(-58)
+    pd.action_right.run_angle(100, 40)
+    pd.drive_base.straight(200)
+    pd.action_right.run_angle(100, 30)
 
 
 if __name__ == "__main__":
