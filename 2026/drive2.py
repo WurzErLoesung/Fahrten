9c4f1630b299ce26e18e7d6c11e1ea6b059078cc
Sebi19 <s.schreitter@gmx.at>
2025-08-20 20:59:50 +0200

    Add drive 2

---
+++ 2026/drive2.py
@@ -0,0 +1,39 @@
+from pybricks.hubs import PrimeHub
+from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor, ForceSensor
+from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
+from pybricks.robotics import DriveBase
+from pybricks.tools import wait, StopWatch, run_task
+from pupdevices_new import PupDevicesNew
+from yaw import Yaw
+
+
+hub = PrimeHub()
+
+print(f"{hub.battery.voltage()/1000} Volt")
+
+watch = StopWatch()
+
+hub.speaker.beep()
+
+def drive2(pd):
+    #DriveBase initialisieren
+    pd.drive_base.use_gyro(False)
+    pd.imu.reset_heading(-90)
+    pd.drive_base.settings(900, 500)
+    yaw = Yaw(hub, pd.left_motor, pd.right_motor)
+    yield True
+
+    pd.action_right.run_angle(5000, 900)
+    pd.action_right.run_until_stalled(-5000, duty_limit=50)
+    pd.drive_base.arc(2000, distance=-595)
+    pd.action_left.run_angle(1500, 60)
+    pd.drive_base.arc(2000, distance=595)
+    
+    yield False
+    print("Fahrt hat " + str(watch.time()/1000) + " Sekunden gedauert.")
+    print(pd.timer.time())
+    watch.reset()
+
+
+if __name__ == "__main__":
+    for element in drive2(PupDevicesNew()): pass
