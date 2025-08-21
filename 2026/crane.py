0c410c33138d9c323f958d01fb054f707cf70795
Sebi19 <s.schreitter@gmx.at>
2025-08-21 11:07:32 +0200

    Adjust for pupdevices

--- 2026/crane.py
+++ 2026/crane.py
@@ -3,7 +3,7 @@ from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor, ForceSenso
 from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
 from pybricks.robotics import DriveBase
 from pybricks.tools import wait, StopWatch, run_task
-from pupdevices_new import PupDevices
+from pupdevices import PupDevices
 from yaw import Yaw
 
 
@@ -20,31 +20,31 @@ def crane(pd):
     pd.drive_base.use_gyro(False)
     pd.imu.reset_heading(-90)
     pd.drive_base.settings(250, 500)
-    yaw = Yaw(hub, pd.left_motor, pd.right_motor)
+    yaw = Yaw(hub, pd.right_motor, pd.left_motor)
     yield True
 
 
-    pd.drive_base.straight(-680)
+    pd.drive_base.straight(680)
     yield True
 
     pd.action_left.run_time(-1000, 1400)
     yield True
 
     yaw(-163)
-    pd.drive_base.straight(80)
-
     pd.drive_base.straight(-80)
 
-    yaw(-112)
-    pd.drive_base.straight(300)
+    pd.drive_base.straight(80)
 
-    pd.drive_base.straight(-100)
+    yaw(-112)
+    pd.drive_base.straight(-300)
+
+    pd.drive_base.straight(100)
 
     yaw(-80)
 
-    pd.drive_base.settings(700)
+    pd.drive_base.settings(-700)
     
-    pd.drive_base.straight(500)
+    pd.drive_base.straight(-500)
 
     
     yield False
