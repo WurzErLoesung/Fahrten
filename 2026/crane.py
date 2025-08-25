1659ca79b26d60b6f946fea3bff2a49ef873bde6
Johni12M <148616249+Johni12M@users.noreply.github.com>
2025-08-25 17:41:06 +0200

    added consistency to crane drive

--- 2026/crane.py
+++ 2026/crane.py
@@ -1,57 +1,68 @@
 from pybricks.hubs import PrimeHub
-from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor, ForceSensor
 from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
+from pybricks.pupdevices import (ColorSensor, ForceSensor, Motor,
+                                 UltrasonicSensor)
 from pybricks.robotics import DriveBase
-from pybricks.tools import wait, StopWatch, run_task
+from pybricks.tools import StopWatch, run_task, wait
+
 from pupdevices import PupDevices
 from yaw import Yaw
 
-
 hub = PrimeHub()
 
-print(f"{hub.battery.voltage()/1000} Volt")
+print(f"Name: {hub.system.name()}, {hub.battery.voltage()/1000} Volt")
 
 watch = StopWatch()
 
 hub.speaker.beep()
 
+
 def crane(pd):
-    #DriveBase initialisieren
+    # DriveBase initialisieren
     pd.drive_base.use_gyro(False)
     pd.imu.reset_heading(-90)
     pd.drive_base.settings(250, 500)
     yaw = Yaw(hub, pd.right_motor, pd.left_motor)
     yield True
 
-
-    pd.drive_base.straight(680)
+    pd.drive_base.straight(630)
     yield True
 
-    pd.action_left.run_time(-1000, 1400)
+    yaw(-110)
     yield True
 
-    yaw(-163)
-    pd.drive_base.straight(-80)
+    pd.drive_base.straight(160)
+    yaw(-90)
+    yield True
 
-    pd.drive_base.straight(80)
+    pd.action_left.run_time(-1000, 1600)
+    yield True
 
-    yaw(-112)
-    pd.drive_base.straight(-300)
+    yaw(-171)
+    pd.drive_base.settings(150)
+    pd.drive_base.straight(-105)
+    yield True
 
-    pd.drive_base.straight(100)
+    pd.drive_base.settings(250)
+    pd.drive_base.straight(150)
+    yield True
 
-    yaw(-80)
+    yaw(-118)
+    pd.drive_base.straight(-305)
+    yield True
 
-    pd.drive_base.settings(-700)
-    
-    pd.drive_base.straight(-500)
+    pd.drive_base.straight(150)
+    yaw(-75)
+    yield True
+    pd.drive_base.settings(700)
+    pd.drive_base.straight(-570)
 
-    
     yield False
-    print("Fahrt hat " + str(watch.time()/1000) + " Sekunden gedauert.")
+    print("Fahrt hat " + str(watch.time() / 1000) + " Sekunden gedauert.")
     print(pd.timer.time())
     watch.reset()
 
 
 if __name__ == "__main__":
-    for element in crane(PupDevices()): pass
+    for element in crane(PupDevices()):
+        pass
