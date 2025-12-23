f95f20652b7243e549119fc43a2854e22fbb5122
MightBeHuman <simon.unger2007@gmail.com>
2025-12-23 17:26:18 +0100

    WIP - NEW: new drive for new module Saves time on way to tasks, overall a little bit more efficient - not functional yet though

---
+++ 2026/forum_new/forum_2026_12_23.py
@@ -0,0 +1,155 @@
+from pupdevices import PupDevices
+from pybricks.hubs import PrimeHub
+from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
+from pybricks.pupdevices import (ColorSensor, ForceSensor, Motor,
+                                 UltrasonicSensor)
+from pybricks.robotics import DriveBase
+from pybricks.tools import StopWatch, run_task, wait
+from yaw import Yaw
+
+hub = PrimeHub()
+print(f"{hub.system.name()}: {hub.battery.voltage()} mV")
+print(f"{hub.system.name()}: {hub.battery.current()} mA")
+watch = StopWatch()
+hub.speaker.beep()
+
+
+def forum_new_new(pd):
+    # DriveBase initialisieren
+    pd.drive_base.use_gyro(True)
+    pd.imu.reset_heading(0)
+    pd.drive_base.settings(700, 500)  # 700, 500
+    yaw = Yaw(
+        hub,
+        pd.right_motor,
+        pd.left_motor,
+        min_velocity=100,
+        max_velocity=500,
+        acceleration=600,
+    )  # 100, 500, 600
+
+    pd.drive_base.straight(200)
+    yaw(-21)
+    pd.drive_base.straight(550)
+    yaw(-80)
+    pd.drive_base.straight(270)
+    yaw(0)
+    pd.drive_base.straight(100)
+    pd.action_right.run_angle(800, -1200)
+    pd.drive_base.straight(-70)
+    yaw(-80)
+    pd.drive_base.straight(350)
+    yaw(-115)
+    pd.drive_base.straight(400)
+    yaw(-180)
+    pd.drive_base.straight(70)
+    pd.action_left.run_angle(100, -50)
+    yield True
+    pd.drive_base.straight(-50)
+    yield True
+    pd.action_right.run_angle(800, 2850)
+    yield True
+    pd.drive_base.straight(80)
+    yield True
+
+
+
+
+
+    pd.action_right.run_angle(800, 1200)
+
+
+
+
+
+
+    yield end(pd)
+    return
+    # Zu Trolley fahren und aufheben
+    pd.drive_base.straight(20)
+    yield True
+    yaw(-19)
+    yield True
+    pd.drive_base.straight(700)
+    yield True
+    yaw(-63)
+    yield True
+    pd.drive_base.straight(145)
+    yield True
+    yaw(-90)
+    yield True
+    pd.drive_base.straight(110)
+    yield True
+    # yaw(-30)
+    # pd.drive_base.straight(-20)
+    # yaw(-50)
+    # pd.drive_base.straight(-90, wait=False)
+    # start = watch.time()
+    # while not pd.drive_base.done():
+    # if watch.time() - start > 1000:
+    # pd.drive_base.stop()
+    # yaw(-55)
+    # pd.drive_base.straight(110)
+    # yaw(0)
+    yaw(0)
+    yield True
+    pd.drive_base.straight(120)
+    yield True
+    pd.action_right.run_angle(800, -1200)
+    yield True
+    # Zu Forum fahren und abladen
+    pd.drive_base.straight(-120)
+    yield True
+    yaw(-90)
+    yield True
+    pd.drive_base.straight(465)
+    yield True
+
+    yaw(-104)
+    yield True
+    pd.drive_base.straight(260)
+    yield True
+    yaw(180)
+    yield True
+    pd.drive_base.straight(60)
+    yield True
+
+    pd.action_left.run_angle(100, -50)
+    yield True
+    pd.drive_base.straight(-50)
+    yield True
+    pd.action_right.run_angle(800, 2850)
+    yield True
+    pd.drive_base.straight(80)
+    yield True
+    pd.drive_base.straight(-60)
+    yield True
+    pd.action_right.run_angle(800, -3000, wait=False)
+    yield True
+
+    yaw(-90)
+    yield True
+    pd.drive_base.straight(145)  # 150
+    yield True
+    yaw(-180)
+    yield True
+    pd.drive_base.straight(30)
+    yield True
+
+    yield False
+    print("Fahrt hat " + str(watch.time() / 1000) + " Sekunden gedauert.")
+    watch.reset()
+
+
+def test(pd):
+    # DriveBase initialisieren
+    pd.drive_base.use_gyro(False)
+    pd.imu.reset_heading(0)
+    pd.drive_base.settings(200, 500)
+    yaw = Yaw(hub, pd.right_motor, pd.left_motor)
+    yield True
+
+
+if __name__ == "__main__":
+    for element in forum_new_new(PupDevices()):
+        pass
