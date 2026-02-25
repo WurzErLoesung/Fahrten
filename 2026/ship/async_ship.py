98e375be935230bdede128fcd3a069719eca2b43
Johni12M <148616249+Johni12M@users.noreply.github.com>
2026-02-25 15:45:33 +0100

    Adjust arc and straight distances in async_ship.py

--- 2026/ship/async_ship.py
+++ 2026/ship/async_ship.py
@@ -20,12 +20,10 @@ async def ship(pd):
     pd.drive_base.settings(500, 500)
     yaw = Yaw(hub, pd.right_motor, pd.left_motor)
 
-    await pd.drive_base.arc(4200, distance=550, wait=True)
-    pd.action_left.run_angle(700, -70)
-    pd.action_left.run_angle(700, 50, wait=True)
+    await pd.drive_base.arc(4000, distance=510)
+    pd.action_left.run_angle(700, 70)
     pd.drive_base.settings(977, 977)
-    await wait(50)
-    await pd.drive_base.straight(-600)
+    await pd.drive_base.straight(-530)
 
     print("Fahrt hat " + str(watch.time() / 1000) + " Sekunden gedauert.")
     # print(pd.timer.time())
