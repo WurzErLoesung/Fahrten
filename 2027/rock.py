8210543855bb38f806c51daa933ff66a9dd1cabd
Johni12M <148616249+Johni12M@users.noreply.github.com>
2026-09-19 10:43:17 +0200

    Update rock.py

--- 2027/rock.py
+++ 2027/rock.py
@@ -9,7 +9,7 @@ from yaw import Yaw
 
 MAX_VOLTAGE = 7000
 USE_GYRO = True
-DRIVE = (500, (400, 400), 300, (500, 500))   # speed, accel, turn_rate, turn_accel
+DRIVE = (500, 300, 300, 500)   # speed, accel, turn_rate, turn_accel
 FAST = (977, 977)
 
 YAW = dict(
@@ -33,13 +33,15 @@ async def mission(pd, yaw):
     db.settings(*DRIVE)
 
     # ---------
-    await multitask(db.straight(450), delayed(500, lambda: pd.action_left.run_angle(170, -200)))
-    await multitask(db.straight(-160), pd.action_left.run_angle(250, -100))
+    await multitask(db.straight(450), delayed(800, lambda: pd.action_left.run_angle(170, -200)))
+    await multitask(db.straight(-160), delayed(60, lambda: pd.action_left.run_angle(400, -120)))
     await pd.action_left.run_angle(500, 300)
     await yaw(-2)
     await wait(50)
-    await multitask(db.straight(167), pd.action_left.run_angle(200, -200))
-    await multitask(db.straight(-550), pd.action_left.run_angle(300, -105))
+    db.settings(400, 300)
+    await multitask(db.straight(163), delayed(250, lambda:pd.action_left.run_angle(200, -200)))
+    db.settings(*DRIVE)
+    await multitask(db.straight(-600), pd.action_left.run_angle(300, -95))
     # ---------
 
     print("Run took " + str(watch.time() / 1000) + " seconds.")
